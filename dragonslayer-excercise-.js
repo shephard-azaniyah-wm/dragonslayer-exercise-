32a7a2b8d0e58b647b87c452c8132c153ce3ae92
Azaniyah Shephard <session1@START-Coding-MBP-C02PP4G9G8WN.local>
2015-09-28 15:26:37 -0700

    no message

--- dragonslayer-excercise-.js
+++ dragonslayer-excercise-.js
@@ -1,3 +1,25 @@
 /**
  * Created by session1 on 9/28/15.
  */
+slaying = 1;
+youHit = Math.floor(Math.random()* 2);
+damageThisRound = (Math.random()* 5 + 1);
+totalDamage = 0
+var slaying = 3;
+while(slaying) {
+    if (youHit) {
+        console.log("You have have poked the dragon successfully! ");
+        totalDamage += damageThisRound;
+        if (totalDamage >= 9) {
+            console.log("Congratulations young warrior you have slain the mighty beast!");
+            slaying = 1;
+        } else {
+            youHit = Math.floor(Math.random() * 2);
+        }
+    } else {
+        console.log("Gah, you have lost my friend. Will you take revenge?");
+        console.log("You have missed your chance to poke the slithery feller!");
+        slaying = Math.floor(Math.random()* 3);
+    }
+
+}
